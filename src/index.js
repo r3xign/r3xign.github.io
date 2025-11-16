95ee0be5ee74ddb6da51c45c97c47baa3b0cc802
r3xign <r3drokc@gmail.com>
2025-11-16 22:17:57 +0930

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+```javascript
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./waves.css";
+import "./birds.css";
+import "./index.css";
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+```
+
+
+---
